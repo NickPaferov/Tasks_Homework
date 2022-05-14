b11ad48c25d9c5da856e964890b3d52d924d7fff
Paferov Nikolai <paferov.nikolai@gmail.com>
2022-05-14 20:09:04 +0300

    add_HW9_newDate_and_setInterval

--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 // import HW11 from '../../h11/HW11';
 import HW7 from '../../h7/HW7'
 import HW8 from '../../h8/HW8'
+import HW9 from '../../h9/HW9'
 // import HW9 from "../../h9/HW9";
 
 function Junior() {
@@ -11,7 +12,7 @@ function Junior() {
             Junior
             <HW7/>
             <HW8/>
-            {/*<HW9/>*/}
+            <HW9/>
             {/*<HW10/>*/}
             {/*<HW11/>*/}
         </div>
--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,29 +3,31 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
+
     const start = () => {
         stop()
-        const id: number = window.setInterval(() => {
-            // setDate
+        const id: number = +setInterval(() => {
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
+
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.toLocaleTimeString() // fix with date
+    const stringDate = date.toLocaleDateString() // fix with date
 
     return (
         <div>
@@ -36,10 +38,12 @@ function Clock() {
                 {stringTime}
             </div>
 
-            {show && (
+            {show ? (
                 <div>
                     {stringDate}
                 </div>
+            ) : (
+                <br/>
             )}
 
             <SuperButton onClick={start}>start</SuperButton>
